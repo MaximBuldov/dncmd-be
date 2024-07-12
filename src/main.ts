3d098f52ea63755e8e4cbf5f651048cc1ec9e6a1
Maxim <Buldov@mail.ru>
2024-07-12 16:58:09 -0700

    update port

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   app.enableCors({
     exposedHeaders: 'Total'
   });
-  await app.listen(process.env.PORT || 4200);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
