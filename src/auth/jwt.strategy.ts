a2815b908aae679fa70f1dcff02ed197db7e3a92
Maxim <Buldov@mail.ru>
2024-07-15 16:43:16 -0700

    deploy setup

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: true,
-      secretOrKey: 'a'
+      secretOrKey: configService.get('JWT_SECRET') || process.env.JWT_SECRET
     });
   }
 
